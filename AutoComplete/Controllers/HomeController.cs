d3d1e5c3bc6b6bef81b808e227bfdb1b9c48f25a
Mohamed Ameerodien <mohamed@amieros.co.za>
2020-09-08 11:01:23 +0200

    Add project files.

---
+++ AutoComplete/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AutoComplete.DAL;
+using AutoComplete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoComplete.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetSearchValues(string search)
+        {
+            MohamedExperimentEntities db = new MohamedExperimentEntities();
+            List<PeopleModel> allsearch = db.PeopleResponses.Where(x => x.Firstname.StartsWith(search)).Select(
+                x => new PeopleModel { Firstname = x.Firstname, Id = x.Id }).ToList();
+
+            return new JsonResult { Data = allsearch, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AutoComplete/Models/PeopleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoComplete.Models
+{
+    public class PeopleModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+    }
+}
